8ce740c9668b517a0f46688a1cf393d0e5f28136
Silence <374619540@qq.com>
2017-01-19 11:59:50 +0800

    Update

--- guide/MovieGuideDemo/MovieGuideDemo/AppDelegate.m
+++ guide/MovieGuideDemo/MovieGuideDemo/AppDelegate.m
@@ -20,7 +20,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     BOOL isFirstLogin = [[[NSUserDefaults standardUserDefaults] objectForKey:@"isFirstLogin"] boolValue];
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds] ;
-    if (isFirstLogin) {
+    if (!isFirstLogin) {
         SIMovieGuide *guide = [[SIMovieGuide alloc]init] ;
         NSString *path = [[NSBundle mainBundle]pathForResource:@"qidong"ofType:@"mp4"] ;
         if (path) {
---
+++ guide/MovieGuideDemo/README.md
@@ -0,0 +1,5 @@
+#视频引导
+视频引导页
+
+
+![img](screenshot.gif)
