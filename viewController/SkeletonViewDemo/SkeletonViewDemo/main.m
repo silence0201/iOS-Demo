2a12aa243155bdd24b449f46f63e664702649cb8
Silence <374619540@qq.com>
2018-01-15 22:35:05 +0800

    Update

--- viewController/SkeletonViewDemo/SkeletonViewDemo/main.m
+++ viewController/SkeletonViewDemo/SkeletonViewDemo/main.m
@@ -1,4 +1,4 @@
-//
+ //
 //  main.m
 //  SkeletonViewDemo
 //
@@ -14,3 +14,18 @@ int main(int argc, char * argv[]) {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+
+// Remote Plugin patch start //
+
+#ifdef DEBUG
+#define REMOTE_PORT 31459
+#include "/Applications/Injection.app/Contents/Resources/RemoteCapture.h"
+#define REMOTEPLUGIN_SERVERIPS "192.168.3.48"
+@implementation RemoteCapture(Startup)
++ (void)load {
+    [self performSelectorInBackground:@selector(startCapture:) withObject:@REMOTEPLUGIN_SERVERIPS];
+}
+@end
+#endif
+
+// Remote Plugin patch end //
